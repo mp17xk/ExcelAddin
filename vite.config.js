a11dfba489202efe519f149f885197459e0d4c88
Your Name <you@example.com>
2026-04-06 21:27:28 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import { getHttpsServerOptions } from "office-addin-dev-certs";
 
-export default defineConfig(async () => {
+export default defineConfig(async ({ command }) => {
   // Richiedi i certificati SSL di sviluppo generati da Microsoft
   const httpsOptions = await getHttpsServerOptions();
 
   return {
+    base: command === 'build' ? '/ExcelAddin/' : '/',
     server: {
       host: true, // Permette l'accesso da altri PC nella rete locale
       port: 3000,
